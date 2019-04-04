002ea0ab8888f294d7f71192bacd076ece0d12f2
Frank Becker <frank@calmdevelopment.de>
2019-04-04 21:36:45 +0200

    fix: add jaxb-api dependency to make plugin compatible with jdk 11
    close #9

---
+++ src/test/groovy/io/sdkman/vendors/PluginSpec.groovy
@@ -0,0 +1,35 @@
+package io.sdkman.vendors
+
+import org.gradle.api.Project
+import org.gradle.testfixtures.ProjectBuilder
+import spock.lang.Specification
+
+
+class PluginSpec extends Specification{
+
+    public static final String PLUGIN_ID = 'io.sdkman.vendors'
+
+    Project project
+
+    def setup() {
+        project = ProjectBuilder.builder().build()
+        project.apply plugin: PLUGIN_ID
+    }
+
+    def "should add tasks"() {
+
+        expect:
+        project.tasks.sdkReleaseVersion instanceof SdkReleaseVersionTask
+        project.tasks.sdkAnnounceVersion instanceof SdkAnnounceVersionTask
+        project.tasks.sdkDefaultVersion instanceof SdkDefaultVersionTask
+        project.tasks.sdkMajorRelease instanceof SdkMajorRelease
+        project.tasks.sdkMinorRelease instanceof SdkMinorRelease
+    }
+
+    def "should add extension"() {
+
+        expect:
+        project.extensions.sdkman
+        project.extensions.sdkman.api == "https://vendors.sdkman.io"
+    }
+}
